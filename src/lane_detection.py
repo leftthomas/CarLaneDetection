b41a9f78cc4265d13b5a6f6486c53201c8393b31
Left Thomas <leftthomas@qq.com>
2017-03-25 16:24:51 +0800

    完成opencv3.2与python3.6的整合

--- src/lane_detection.py
+++ src/lane_detection.py
@@ -1,3 +1,10 @@
+import cv2
 import matplotlib.image as mplimg
 
 img = mplimg.imread('../resources/lane.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+cv2.imshow('gr', gray)
+# 程序暂停
+cv2.waitKey(0)
+# Note that if you use cv2.imread() to read image, the image will
+# be in format BGR.
